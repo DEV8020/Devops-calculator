75a0d3481528f7044dd2b89ba19557b9a850aec0
devdatt <devdatt.datar@iiitb.ac.in>
2023-03-19 20:37:34 +0530

    modified

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -71,84 +71,84 @@ public class Main {
 
                     break;
 
-//                case 2:
-//                    // find square root
-//                    d_number1 = -1;
-//
-//                    // Keep executing do-while loop till user enters a non-negative number
-//                    while (d_number1 < 0) {
-//                        try {
-//                            System.out.print("Enter a non-negative number: ");
-//                            d_number1 = scanner.nextDouble();
-//
-//                            if (d_number1 < 0)
-//                                System.out.println("Square Root is only applicable for non-negative numbers");
-//                            else
-//                                System.out.println("Square Root of " + d_number1 + " is: " + calculator.squareRoot(d_number1) + "\n");
-//                        }
-//                        // To handle cases where user enters non-numeric input
-//                        catch (InputMismatchException e) {
-//                            System.out.println("Square Root is only applicable for non-negative numbers");
-//                        }
-//                        scanner.nextLine(); // Clear the buffer to receive new input
-//                    }
-//
-//                    break;
-//
-//                case 3:
-//                    // find power
-//                    d_number1 = Double.POSITIVE_INFINITY; d_number2 = Double.POSITIVE_INFINITY;
-//                    while (d_number1 == Double.POSITIVE_INFINITY) {
-//                        try {
-//                            System.out.print("Enter the first number: ");
-//                            d_number1 = scanner.nextDouble();
-//                        }
-//                        // To handle cases where user enters non-numeric input
-//                        catch (InputMismatchException e) {
-//                            System.out.println("Non-numeric input is not valid");
-//                        }
-//                        scanner.nextLine(); // Clear the buffer to receive new input
-//                    }
-//                    while (d_number2 == Double.POSITIVE_INFINITY) {
-//                        try {
-//                            System.out.print("Enter the second number: ");
-//                            d_number2 = scanner.nextDouble();
-//                        }
-//                        // To handle cases where user enters non-numeric input
-//                        catch (InputMismatchException e) {
-//                            System.out.println("Non-numeric input is not valid");
-//                        }
-//                        scanner.nextLine(); // Clear the buffer to receive new input
-//                    }
-//
-//                    System.out.println(d_number1 + " raised to " + d_number2 + " is: " + calculator.power(d_number1, d_number2));
-//                    System.out.println("\n");
-//                    break;
-//
-//                case 4:
-//                    // find natural log
-//                    d_number1 = 0;
-//
-//                    // Keep executing do-while loop till user enters a positive number
-//                    while (d_number1 <= 0) {
-//                        try {
-//                            System.out.print("Enter a positive number: ");
-//                            d_number1 = scanner.nextDouble();
-//
-//                            if (d_number1 <= 0)
-//                                System.out.println("Natural Logarithm is only applicable for positive numbers");
-//                            else
-//                                System.out.println("Natural Logarithm of " + d_number1 + " is: " + calculator.naturalLog(d_number1) + "\n");
-//                        }
-//                        // To handle cases where user enters non-numeric input
-//                        catch (InputMismatchException e) {
-//                            System.out.println("Natural Logarithm is only applicable for positive numbers");
-//                        }
-//                        scanner.nextLine(); // Clear the buffer to receive new input
-//                    }
-//
-//                    break;
-//
+                case 2:
+                    // find square root
+                    d_number1 = -1;
+
+                    // Keep executing do-while loop till user enters a non-negative number
+                    while (d_number1 < 0) {
+                        try {
+                            System.out.print("Enter a non-negative number: ");
+                            d_number1 = scanner.nextDouble();
+
+                            if (d_number1 < 0)
+                                System.out.println("Square Root is only applicable for non-negative numbers");
+                            else
+                                System.out.println("Square Root of " + d_number1 + " is: " + calculator.squareRoot(d_number1) + "\n");
+                        }
+                        // To handle cases where user enters non-numeric input
+                        catch (InputMismatchException e) {
+                            System.out.println("Square Root is only applicable for non-negative numbers");
+                        }
+                        scanner.nextLine(); // Clear the buffer to receive new input
+                    }
+
+                    break;
+
+                case 3:
+                    // find power
+                    d_number1 = Double.POSITIVE_INFINITY; d_number2 = Double.POSITIVE_INFINITY;
+                    while (d_number1 == Double.POSITIVE_INFINITY) {
+                        try {
+                            System.out.print("Enter the first number: ");
+                            d_number1 = scanner.nextDouble();
+                        }
+                        // To handle cases where user enters non-numeric input
+                        catch (InputMismatchException e) {
+                            System.out.println("Non-numeric input is not valid");
+                        }
+                        scanner.nextLine(); // Clear the buffer to receive new input
+                    }
+                    while (d_number2 == Double.POSITIVE_INFINITY) {
+                        try {
+                            System.out.print("Enter the second number: ");
+                            d_number2 = scanner.nextDouble();
+                        }
+                        // To handle cases where user enters non-numeric input
+                        catch (InputMismatchException e) {
+                            System.out.println("Non-numeric input is not valid");
+                        }
+                        scanner.nextLine(); // Clear the buffer to receive new input
+                    }
+
+                    System.out.println(d_number1 + " raised to " + d_number2 + " is: " + calculator.power(d_number1, d_number2));
+                    System.out.println("\n");
+                    break;
+
+                case 4:
+                    // find natural log
+                    d_number1 = 0;
+
+                    // Keep executing do-while loop till user enters a positive number
+                    while (d_number1 <= 0) {
+                        try {
+                            System.out.print("Enter a positive number: ");
+                            d_number1 = scanner.nextDouble();
+
+                            if (d_number1 <= 0)
+                                System.out.println("Natural Logarithm is only applicable for positive numbers");
+                            else
+                                System.out.println("Natural Logarithm of " + d_number1 + " is: " + calculator.naturalLog(d_number1) + "\n");
+                        }
+                        // To handle cases where user enters non-numeric input
+                        catch (InputMismatchException e) {
+                            System.out.println("Natural Logarithm is only applicable for positive numbers");
+                        }
+                        scanner.nextLine(); // Clear the buffer to receive new input
+                    }
+
+                    break;
+
                 default:
                     System.out.println("Exiting....");
                     scanner.close();
@@ -168,36 +168,36 @@ public class Main {
         return result;
     }
 
-//    public double squareRoot(double number1) {
-//        logger.info("[SQ ROOT] - " + number1);
-//        double result = Math.sqrt(number1);
-//        logger.info("[RESULT - SQ ROOT] - " + result);
-//        return result;
-//    }
-//
-//    public double power(double number1, double number2) {
-//        logger.info("[POWER - " + number1 + " RAISED TO] " + number2);
-//        double result = Math.pow(number1,number2);
-//        logger.info("[RESULT - POWER] - " + result);
-//        return result;
-//    }
-//
-//    public double naturalLog(double number1) {
-//        logger.info("[NATURAL LOG] - " + number1);
-//        double result = 0;
-//
-//        try {
-//            if (number1 < 0) {
-//                result = Double.NaN;
-//                throw new ArithmeticException("Case of NaN 0.0/0.0");
-//            }
-//            else
-//                result = Math.log(number1);
-//        }
-//        catch (ArithmeticException error) {
-//            System.out.println("[EXCEPTION - LOG] - Cannot find log of negative numbers " + error.getLocalizedMessage());
-//        }
-//        logger.info("[RESULT - NATURAL LOG] - " + result);
-//       return result;
- // }
+    public double squareRoot(double number1) {
+        logger.info("[SQ ROOT] - " + number1);
+        double result = Math.sqrt(number1);
+        logger.info("[RESULT - SQ ROOT] - " + result);
+        return result;
+    }
+
+    public double power(double number1, double number2) {
+        logger.info("[POWER - " + number1 + " RAISED TO] " + number2);
+        double result = Math.pow(number1,number2);
+        logger.info("[RESULT - POWER] - " + result);
+        return result;
+    }
+
+    public double naturalLog(double number1) {
+        logger.info("[NATURAL LOG] - " + number1);
+        double result = 0;
+
+        try {
+            if (number1 < 0) {
+                result = Double.NaN;
+                throw new ArithmeticException("Case of NaN 0.0/0.0");
+            }
+            else
+                result = Math.log(number1);
+        }
+        catch (ArithmeticException error) {
+            System.out.println("[EXCEPTION - LOG] - Cannot find log of negative numbers " + error.getLocalizedMessage());
+        }
+        logger.info("[RESULT - NATURAL LOG] - " + result);
+       return result;
+  }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -8,6 +8,8 @@ public class CalculatorTest {
 
     @Test
     public void factorialTruePositive() {
+
+
         assertEquals("Finding factorial of a number for True Positive", 6, calculator.factorial(3), DELTA);
         assertEquals("Finding factorial of a number for True Positive", 24, calculator.factorial(4), DELTA);
     }
@@ -19,41 +21,41 @@ public class CalculatorTest {
     }
     //fact ends
 
-//    @Test
-//    public void powerTruePositive() {
-//        assertEquals("Finding power for True Positive", 32, calculator.power(2, 5), DELTA);
-//        assertEquals("Finding power for True Positive", 81, calculator.power(9, 2), DELTA);
-//    }
-//
-//    @Test
-//    public void powerTrueNegative() {
-//        assertNotEquals("Finding power for True Negative", 69, calculator.power(2, 2), DELTA);
-//        assertNotEquals("Finding power for True Negative", -69420, calculator.power(-2, 20), DELTA);
-//    }
-//
-//    @Test
-//    public void logTruePositive() {
-//        assertEquals("Finding natural log for True Positive", 0, calculator.naturalLog(1), DELTA);
-//        assertEquals("Finding natural log for True Positive", 5.703782474656201, calculator.naturalLog(300), DELTA);
-//    }
-//
-//    @Test
-//    public void logTrueNegative() {
-//        assertNotEquals("Finding natural log for True Negative", 69, calculator.naturalLog(2.4), DELTA);
-//        assertNotEquals("Finding natural log for True Negative", 420, calculator.naturalLog(2.1), DELTA);
-//    }
-//
-//    @Test
-//    public void sqrootTruePositive() {
-//        assertEquals("Finding square root for True Positive", 11, calculator.squareRoot(121), DELTA);
-//        assertEquals("Finding square root for True Positive", 121, calculator.squareRoot(14641), DELTA);
-//    }
-//
-//    @Test
-//    public void sqrootTrueNegative() {
-//        assertNotEquals("Finding square root for True Negative", 69, calculator.squareRoot(3), DELTA);
-//        assertNotEquals("Finding square root for True Negative", -42, calculator.squareRoot(4), DELTA);
-//    }
+    @Test
+    public void powerTruePositive() {
+        assertEquals("Finding power for True Positive", 32, calculator.power(2, 5), DELTA);
+        assertEquals("Finding power for True Positive", 81, calculator.power(9, 2), DELTA);
+    }
+
+    @Test
+    public void powerTrueNegative() {
+        assertNotEquals("Finding power for True Negative", 69, calculator.power(2, 2), DELTA);
+        assertNotEquals("Finding power for True Negative", -69420, calculator.power(-2, 20), DELTA);
+    }
+
+    @Test
+    public void logTruePositive() {
+        assertEquals("Finding natural log for True Positive", 0, calculator.naturalLog(1), DELTA);
+        assertEquals("Finding natural log for True Positive", 5.703782474656201, calculator.naturalLog(300), DELTA);
+    }
+
+    @Test
+    public void logTrueNegative() {
+        assertNotEquals("Finding natural log for True Negative", 69, calculator.naturalLog(2.4), DELTA);
+        assertNotEquals("Finding natural log for True Negative", 420, calculator.naturalLog(2.1), DELTA);
+    }
+
+    @Test
+    public void sqrootTruePositive() {
+        assertEquals("Finding square root for True Positive", 11, calculator.squareRoot(121), DELTA);
+        assertEquals("Finding square root for True Positive", 121, calculator.squareRoot(14641), DELTA);
+    }
+
+    @Test
+    public void sqrootTrueNegative() {
+        assertNotEquals("Finding square root for True Negative", 69, calculator.squareRoot(3), DELTA);
+        assertNotEquals("Finding square root for True Negative", -42, calculator.squareRoot(4), DELTA);
+    }
 
 
 }
